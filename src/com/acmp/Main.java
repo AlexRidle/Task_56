09c493aed4fd081d22e5f316e00aa627b4849c1d
Alexander <AlexanderBely98@gmail.com>
2018-11-15 18:44:44 +0300

    Initial commit

---
+++ src/com/acmp/Main.java
@@ -0,0 +1,86 @@
+package com.acmp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    private static ArrayList<String> yourFriends = new ArrayList<>();
+    private static ArrayList<String> usersWhoAddedYou = new ArrayList<>();
+    private static ArrayList<String> mutualFriends = new ArrayList<>();
+    private static ArrayList<String> alsoFriendOf = new ArrayList<>();
+
+    public static void main(String[] args) {
+        try {
+            readFile();
+            findMutualFriends();
+            writeFile();
+        } catch (IOException | TaskException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readFile() throws IOException, TaskException {
+        File file = new File("INPUT.txt");
+        Scanner inputFile = new Scanner(file);
+        int yourFriendlistCount = inputFile.nextInt();
+        for(int i = 0; i < yourFriendlistCount; i++){
+            yourFriends.add(inputFile.next());
+        }
+        int youreInTheFriendlistCount = inputFile.nextInt();
+        for(int i = 0; i < youreInTheFriendlistCount; i++){
+            usersWhoAddedYou.add(inputFile.next());
+        }
+        Collections.sort(yourFriends);
+        Collections.sort(usersWhoAddedYou);
+        if (!(0 <= yourFriendlistCount || yourFriendlistCount <= 200 || 0 <= youreInTheFriendlistCount || youreInTheFriendlistCount <= 200)) {
+            throw new TaskException("Please, check INPUT.txt file. Friends cant be less than 0 or higher than 200");
+        }
+    }
+
+    private static void findMutualFriends(){
+        for(String friendName : usersWhoAddedYou){
+            if (yourFriends.contains(friendName)) {
+                mutualFriends.add(friendName);
+            } else {
+                alsoFriendOf.add(friendName);
+            }
+        }
+    }
+
+    private static void writeFile() {
+        File file = new File("OUTPUT.txt");
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(file);
+            writer.println("Friends: " + Arrays.toString(yourFriends.toArray())
+                    .replace("[","")
+                    .replace("]",""));
+            writer.println("Mutual Friends: " + Arrays.toString(mutualFriends.toArray())
+                    .replace("[","")
+                    .replace("]",""));
+            writer.println("Also Friend of: " + Arrays.toString(alsoFriendOf.toArray())
+                    .replace("[","")
+                    .replace("]",""));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            writer.flush();
+            writer.close();
+        }
+    }
+}
+
+class TaskException extends Exception {
+
+    public TaskException(String message) {
+        super(message);
+    }
+
+}
